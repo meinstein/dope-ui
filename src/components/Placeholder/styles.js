50487140cd09fe2016f90500b76e86e3f0076081
meinstein <max@datascience.com>
2018-03-12 22:43:15 -0700

    test

---
+++ src/components/Placeholder/styles.js
@@ -0,0 +1,8 @@
+export default {
+  background: 'linear-gradient(135deg, #ffffff, #d3d3d3)',
+  backgroundSize: '200% 200%',
+  transition: 'background-position 1500ms ease-in-out',
+  width: '100%',
+  height: '100%',
+  boxShadow: '0 3px 20px 0 rgba(0, 0, 0, 0.05), 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 0 1px 0 rgba(0, 0, 1, 0.1)'
+}
---
+++ src/examples/Placeholder/ExampleOne.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Placeholder } from '../../../lib'
+// import { Placeholder } from 'dope-ui'
+
+const PlaceholderExample = () => <Placeholder />
+
+export default PlaceholderExample
